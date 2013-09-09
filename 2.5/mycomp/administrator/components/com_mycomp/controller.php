e3831aa667a955ef5c1827c66930d37c12af8559
digioz <digioz@gmail.com>
2013-09-08 19:37:49 -0500

    Added 48x48 Backend Top Menu Icons to the Task Pages

--- 2.5/mycomp/administrator/components/com_mycomp/controller.php
+++ 2.5/mycomp/administrator/components/com_mycomp/controller.php
@@ -5,25 +5,32 @@ jimport('joomla.application.componenet.controller');
 class MyCompController extends JController {
 
 	function display() {
-		JToolBarHelper::Title('My Component');
+		$doc=JFactory::getDocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolBarHelper::Title('My Component','mycomp.png');
 		echo JText::_('COM_MYCOMP_WELCOME');
 	}
 
 	function create() {
 		$doc=JFactory::getDocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
 		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/backend_create_task.css');
 		$doc->addScript(JURI::root().'media/com_mycomp/js/backend_create_task.js');
-		JToolBarHelper::Title('Create New Task');
+		JToolBarHelper::Title('Create New Task','createtask.png');
 		echo '<div id="welcome">'.JText::_('COM_MYCOMP_TASK_CREATE').'</div>';
 	}
 	
 	function listtask() {
-		JToolBarHelper::Title('List All Tasks');
+		$doc=JFactory::getDocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolBarHelper::Title('List All Tasks','listtask.png');
 		echo JText::_('COM_MYCOMP_TASK_LISTTASK');
 	}
 	
 	function help() {
-		JToolBarHelper::Title('Help Documentation');
+		$doc=JFactory::getDocument();
+		$doc->addStyleSheet(JURI::root().'media/com_mycomp/css/mycomp.css');
+		JToolBarHelper::Title('Help Documentation','helptask.png');
 		echo JText::_('COM_MYCOMP_TASK_HELP');
 		echo '<h1>This is the Help Task <input type="text">';
 	}
